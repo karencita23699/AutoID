d783c556b2b4fe05277f9387c614adb1e5ddce42
karencita23699 <95319143+karencita23699@users.noreply.github.com>
2024-05-23 02:58:29 -0400

    Update ApiClient.java

--- app/src/main/java/com/example/autoid/ApiClient.java
+++ app/src/main/java/com/example/autoid/ApiClient.java
@@ -9,20 +9,29 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/**
+ * ApiClient es una clase que configura y proporciona una instancia de Retrofit para realizar
+ * solicitudes HTTP a un servidor REST.
+ */
 public class ApiClient {
-    private final Retrofit retrofit;
-    private final HttpLoggingInterceptor loggin;
+    private final Retrofit retrofit; // Instancia de Retrofit
+    private final HttpLoggingInterceptor loggin; // Interceptor de logging para registrar detalles de las solicitudes HTTP
 
+    /**
+     * Constructor de ApiClient que configura Retrofit con un interceptor de logging y tiempos de espera.
+     */
     public ApiClient() {
         loggin = new HttpLoggingInterceptor();
-        loggin.setLevel(HttpLoggingInterceptor.Level.BODY);
+        loggin.setLevel(HttpLoggingInterceptor.Level.BODY); // Configura el nivel de logging
+
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
         httpClient.addInterceptor(loggin)
-                .connectTimeout(30, TimeUnit.SECONDS)
-                .writeTimeout(30, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
-                .callTimeout(30, TimeUnit.SECONDS);
+                .connectTimeout(30, TimeUnit.SECONDS) // Tiempo de espera para la conexión
+                .writeTimeout(30, TimeUnit.SECONDS) // Tiempo de espera para la escritura
+                .readTimeout(30, TimeUnit.SECONDS) // Tiempo de espera para la lectura
+                .callTimeout(30, TimeUnit.SECONDS); // Tiempo de espera para la llamada
 
+        // Configura Retrofit con la URL base y el convertidor de JSON a objetos
         retrofit = new Retrofit.Builder()
                 .baseUrl(Base_Url)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -30,6 +39,11 @@ public class ApiClient {
                 .build();
     }
 
+    /**
+     * Proporciona la instancia de Retrofit configurada.
+     *
+     * @return La instancia de Retrofit.
+     */
     public Retrofit getRetrofit() {
         return retrofit;
     }
